8c91e7bb4b39c4deb15d68170a5ef82646e4300c
max.segale <max.segale@fcbcure.com>
2021-07-08 08:33:42 -0400

    Added init function

--- app/index.js
+++ app/index.js
@@ -2,8 +2,6 @@ import { fileList } from './files.js';
 import { pdfContent } from './pdf.js';
 import { readStub } from './reader.js';
 
-const path = '/Users/maxwell/Documents/paystubs';
-
 function payDisplay(stubList) {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -16,19 +14,23 @@ function payDisplay(stubList) {
   });
 }
 
-fileList(path, 'pdf').then((files) => {
-  const stubList = [];
-  const promises = [];
-  for (const file of files) {
-    let promise = pdfContent(path, file).then((content) => {
-      const paystub = readStub(content);
-      stubList.push(paystub);
-    });
-    promises.push(promise);
-  }
-  Promise.all(promises).then(() => {
-    return stubList.sort((a, b) => {
-      return a.date - b.date;
-    });
-  }).then(payDisplay);
-});
+const init = (path) => {
+  fileList(path, 'pdf').then((files) => {
+    const stubList = [];
+    const promises = [];
+    for (const file of files) {
+      let promise = pdfContent(path, file).then((content) => {
+        const paystub = readStub(content);
+        stubList.push(paystub);
+      });
+      promises.push(promise);
+    }
+    Promise.all(promises).then(() => {
+      return stubList.sort((a, b) => {
+        return a.date - b.date;
+      });
+    }).then(payDisplay);
+  });
+};
+
+init('/Users/maxwell/Documents/paystubs');
