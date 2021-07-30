7415d239e6a793c6b83259a39da844f62d71ddd7
max.segale <max.segale@fcbcure.com>
2021-07-30 08:45:03 -0400

    Improve functions

--- app/index.js
+++ app/index.js
@@ -4,35 +4,37 @@ import { readStub } from './reader.js';
 import { datePeriods, dateMatch } from './dates.js';
 import { payDisplay } from './display.js';
 
-(function () {
-  const path = '/Users/maxwell/Documents/Paystubs';
-  const type = 'pdf';
-  const interval = 'month';
-  const paystubs = [];
-  const addStub = (content) => {
-    const paystub = readStub(content);
-    paystubs.push(paystub);
-  };
-  const sortDates = () => {
-    paystubs.sort((a, b) => a.date - b.date);
-    const firstDate = paystubs[0].date.valueOf();
-    const lastDate = paystubs[paystubs.length - 1].date.valueOf();
-    const payPeriods = datePeriods(firstDate, lastDate, interval);
-    return dateMatch(paystubs, payPeriods);
-  };
-  const processContent = () => {
-    const payDates = sortDates();
-    payDisplay(payDates, interval);
-  };
-  const readFiles = (files) => {
-    const promises = [];
-    for (const file of files) {
-      let promise = pdfContent(path, file).then(addStub);
-      promises.push(promise);
-    }
-    Promise.all(promises).then(processContent);
-  };
+const path = '/Users/maxwell/Documents/Paystubs';
+const type = 'pdf';
+const interval = 'month';
+const paystubs = [];
 
-  // Get files in directory, read content
-  fileList(path, type).then(readFiles);
-}());
+function payDateSort() {
+  paystubs.sort((a, b) => a.date - b.date);
+  const firstDate = paystubs[0].date.valueOf();
+  const lastDate = paystubs[paystubs.length - 1].date.valueOf();
+  const payPeriods = datePeriods(firstDate, lastDate, interval);
+  return dateMatch(paystubs, payPeriods);
+}
+
+function addStub(content) {
+  const paystub = readStub(content);
+  paystubs.push(paystub);
+}
+
+function handleStubs() {
+  const payDates = payDateSort();
+  payDisplay(payDates, interval);
+}
+
+function fileRead(files) {
+  const promises = [];
+  for (const file of files) {
+    let promise = pdfContent(path, file).then(addStub);
+    promises.push(promise);
+  }
+  Promise.all(promises).then(handleStubs);
+}
+
+// Get files in directory, read content
+fileList(path, type).then(fileRead);
